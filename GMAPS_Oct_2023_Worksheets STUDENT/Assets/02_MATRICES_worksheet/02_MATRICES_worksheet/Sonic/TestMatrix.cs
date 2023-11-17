bac816df2a4d542b2ea28741f0a979335f0d617e
Ian Wong <ianw35091@gmail.com>
2023-11-17 18:45:24 +0800

    HMatrix2D (3)
    Made it use the correct files.

--- GMAPS_Oct_2023_Worksheets STUDENT/Assets/02_MATRICES_worksheet/02_MATRICES_worksheet/Sonic/TestMatrix.cs
+++ GMAPS_Oct_2023_Worksheets STUDENT/Assets/02_MATRICES_worksheet/02_MATRICES_worksheet/Sonic/TestMatrix.cs
@@ -1,35 +1,16 @@
-// Uncomment this whole file.
+//// Uncomment this whole file.
 
-using System.Collections;
-using System.Collections.Generic;
-using Unity.Collections;
-using UnityEngine;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
 
-public class TestMatrix : MonoBehaviour
-{
-    private HMatrix2D mat = new HMatrix2D();
-    private HMatrix2D mat1;
-    private HMatrix2D mat2;
-    private HMatrix2D resultMat;
-    private HVector2D vec1;
-    private HVector2D resultMatVec;
+//public class TestMatrix : MonoBehaviour
+//{
+//    private HMatrix2D mat = new HMatrix2D();
 
-    void Start()
-    {
-        Question2();
-        //mat.setIdentity();
-        //mat.Print();
-    }
+//    void Start()
+//    {
 
-    void Question2()
-    {
-        mat1 = new HMatrix2D(1, 4, 2,
-            3, 3, 6, 
-            8, 5, 0);
-        mat2 = new HMatrix2D(2, 5, 1,
-            6, 7, 1,
-            1, 8, 1);
-        resultMat = mat1 * mat2;
-        resultMat.Print();
-    }
-}
+//    }
+
+//}
